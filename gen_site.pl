e6ab0ec9220323739400cbe700daf2c395e7a3a3
John D. Leary Jr <git@jleary.cc>
2018-08-08 21:38:04 -0400

    Removed redundant config variables.  Added an initialization function.

--- gen_site.pl
+++ gen_site.pl
@@ -7,25 +7,30 @@ use Browser::Open qw(open_browser);
 # Written by:   (John Leary)[git@jleary.cc]
 # Date Created: 28 Jul 2018
 # Version:      08 Aug 2018
-# Dependencies: pandoc, perl, Config::Tiny,Browser::Open, and rsync
-# Deb Packages: pandoc, perl, libconfig-tiny-perl,libbrowser-open-perl, rsync
+# Dependencies: pandoc, perl, Config::Tiny, Browser::Open, and rsync
+# Deb Packages: pandoc, perl, libconfig-tiny-perl, libbrowser-open-perl, rsync
+# Todo:
+##    - make fully platform agnostic (replace mini_httpd,deal w/ paths),
+##    - replace recursion with loop and stack
 
-my $site    = defined $ARGV[1]? '/'.$ARGV[1] :  '';
-my $cfg     = Config::Tiny->read("$ENV{'HOME'}/Site$site/site.cfg") or die "Could not open site.cfg";
+my $site    = defined $ARGV[1]? '/'. $ARGV[1] :  '';
 
 ## Constants
-my $basedir = "$ENV{'HOME'}/Site";
-my $srcdir  = "$basedir/src";
-my $outdir  = "$basedir/out";
-my $incdir  = "$basedir/inc";
-## Settings Defined in cfg
-my $srvurl  = $cfg->{_}->{"remote"} or die "remote not defined in site.cfg";
-my $prefix  = $cfg->{_}->{"prefix"} or die "prefix not defined in site.cfg";
-my $tabmap  = $cfg->{'tabmap'}      or die "[tabmap] not specified in site.cfg";
-my $secure  = $cfg->{'secure'};
+my $BASEDIR = "$ENV{'HOME'}/Site$site";
+my $SRCDIR  = "$BASEDIR/src";
+my $OUTDIR  = "$BASEDIR/out";
+my $INCDIR  = "$BASEDIR/inc";
+
+&init() and exit if $ARGV[0] eq '-i'; 
+
+## Required Settings
+my $cfg     = Config::Tiny->read("$ENV{'HOME'}/Site$site/site.cfg") or die "Could not open site.cfg";
+die "remote not defined in site.cfg" if !defined $cfg->{_}->{'remote'};
+die "prefix not defined in site.cfg" if !defined $cfg->{_}->{'prefix'};
+die "tabmap not defined in site.cfg" if !defined $cfg->{'tabmap'};
 
 my %subs=(
-    '-g'=>[\&gen_site,$srcdir],
+    '-g'=>[\&gen_site,$SRCDIR],
     '-p'=>[\&push    ,undef  ],
     '-n'=>[\&new     ,undef  ],
     '-v'=>[\&view    ,undef  ],
@@ -39,53 +44,52 @@ $subs{$arg}->[0]($subs{$arg}->[1]);
 
 sub gen_site{
     (my $regex, my $year);
-    if($srcdir eq $_[0]){
+    if($SRCDIR eq $_[0]){ #Base condition
         $year    = (localtime)[5] + 1900;
         $regex   =''; #tab regex
-        ($regex .= "$_|") foreach keys %{$tabmap};
-        $regex   =~s#\/#\\/#g; 
+        ($regex .= "$_|") foreach keys %{$cfg->{'tabmap'}};
+        $regex   =~s#(\/|\.)#\\$1#g; 
         chop($regex);
     }else{
         (undef,$regex,$year) = @_;
     }
     return if $_[0] =~ /\.git$/;
-    (my $newdir = $_[0]) =~ s/$srcdir/$outdir/g;
+    (my $newdir = $_[0]) =~ s/$SRCDIR/$OUTDIR/g;
     print "Make Dir: $newdir\n";
     mkdir $newdir;
     foreach(<"$_[0]*">){
         print "Recursing On Directory: $_\n" and &gen_site("$_/",$regex,$year) and next if(-d $_);
-        (my $file = $_) =~ s/$srcdir/$outdir/g;
-        $_ =~ /^$srcdir\/($regex).*/g;
+        (my $file = $_) =~ s/$SRCDIR/$OUTDIR/g;
+        $_ =~ /^$SRCDIR\/($regex).*/g;
         my $tab = 'none';
-        my $args = '';
+        my $login = '';
         if(defined $1){
-            $tab  =  $tabmap->{$1};# if defined $1;
-            $args = '-V login=login' if (defined $secure->{$1} && $secure->{$1} eq 'true');
+            $tab   =  $cfg->{'tabmap'}->{$1};# if defined $1;
+            $login = '-V login=login' if (defined $cfg->{'secure'}->{$1} && $cfg->{'secure'}->{$1} eq 'true');
         }
         $file =~ s/\.md$/.html/g;
         if($_ =~ /\.(md|html)$/){
             print "Processing: $_ -> $file\n";
-            #Possible log hash of file skip here unless md file or template changes
-            print `pandoc -s --template=$incdir/template.html $args -T $prefix -V year=$year -V lang=en -V tab=$tab -i $_ -o $file`;
+            print `pandoc -s --template=$INCDIR/template.html $login -T "$cfg->{_}->{'prefix'}" -V year=$year -V lang=en -V tab=$tab -i $_ -o $file`;
         }
     }
 }
 
 sub push{
-    chdir $srcdir  or die "Could not chdir into $srcdir\n";
+    chdir $SRCDIR  or die "Could not chdir into $SRCDIR\n";
     print "Commit & Publish y/N: ";
     print "Exiting...\n" and exit if(<STDIN>!~ /^[Y|y]/);
-    if(defined $cfg->{_}->{"usegit"} && $cfg->{_}->{"usegit"} eq 'true'){
+    if(defined $cfg->{_}->{"usegit"} && lc($cfg->{_}->{"usegit"}) eq 'true'){
         print "Commiting Source to Git\n",;
         print `git commit -a -m "Automatic site update on push"`;
     }
     print "Pushing Site\n";
-    print `rsync -avz --progress -e "ssh" $outdir/ $srvurl`;
+    print `rsync -avz --progress -e "ssh" $OUTDIR/ $cfg->{_}->{"remote"}`;
 }
 
 sub new{
-    stat $srcdir or die "Could not stat $srcdir\n";
-    stat $outdir or die "Could not stat $outdir\n";
+    stat $SRCDIR or die "Could not stat $SRCDIR\n";
+    stat $OUTDIR or die "Could not stat $OUTDIR\n";
     my $name = '';
     while($name eq ''){
         print "What would you like to name the post (CTRL-C To Exit): ";
@@ -93,19 +97,42 @@ sub new{
         chomp($name);
         $name = '' if $name eq 'index';
     }
-    open(NEW,"+>","$srcdir/posts/$name.md") or die "Could not create post: $name.md\n";
+    open(NEW,"+>","$SRCDIR/posts/$name.md") or die "Could not create post: $name.md\n";
     close NEW;
     if (defined $cfg->{_}->{"usegit"} && lc($cfg->{_}->{"usegit"}) eq 'true'){
-        chdir $srcdir;
-        print `git add "$srcdir/posts/$name.md"` ; 
+        chdir $SRCDIR;
+        print `git add "$SRCDIR/posts/$name.md"` ; 
     }
-    mkdir "$outdir/media/posts/$name" or die "Could not creat post: $srcdir/media/posts/$name";
+    mkdir "$OUTDIR/media/posts/$name" or die "Could not creat post: $SRCDIR/media/posts/$name";
 }
 
+sub init{
+    print "Directory exists... Exiting.\n" and exit if (-d $BASEDIR);
+    print "Make Dir: base directory\n"     and mkdir $BASEDIR         or die "Could not create base directory";
+    print "Make Dir: source directory\n"   and mkdir $SRCDIR          or die "Could not create source directory";
+    print "Make Dir: posts directory\n"    and mkdir "$SRCDIR/posts"  or die "Could not create source directory";
+    print "Make Dir: output directory\n"   and mkdir $OUTDIR          or die "Could not create output directory";
+    print "Make Dir: includes directory\n" and mkdir $INCDIR          or die "Could not create includes directory";
+    print "Write File: template.html\n"    and `pandoc -D html > $INCDIR/template.html`;
+    print "Write File: site.cfg\n" and open(my $sc, '>', "$BASEDIR/site.cfg") or die "Could not open site.cfg for writing.";
+    print $sc <<EOF;
+;remote   = username\@rsync.example.com
+;prefix   = Title Of Site
+;usegit   = false ; whether or not you want to perform a git commit on the directory
+;[tabmap]
+;;file location mapped to tab name
+;index.md    = home
+;login/      = login
+;[secure]
+;;list of login tab keys
+;;login/     = true
+EOF
+    close $sc;
+
+}
 sub view{
     print "Open Url: http://localhost:8000\n";
-    system("mini_httpd -p 8000 -d $outdir -h localhost 2>&1 > /dev/null &");
-#    system("xdg-open http://localhost:8000");
+    system("mini_httpd -p 8000 -d $OUTDIR -h localhost 2>&1 > /dev/null &");
     open_browser('http://localhost:8000');
     print "Press [enter] to stop server: ";
     my $a = <STDIN>;
@@ -114,6 +141,7 @@ sub view{
 
 sub help{
      print <<HELP;
+    -i: initializes a site
     -g: generates site
     -p: pushes site
     -n: new post
